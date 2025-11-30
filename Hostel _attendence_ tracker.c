87d97d9727a92a0a413c3e4ba92dc19297197c96
MrigangeeSarma5623 <mrigangeesarma@gmail.com>
2025-11-30 21:52:57 +0530

    Add files via upload

---
+++ Hostel _attendence_ tracker.c
@@ -0,0 +1,95 @@
+/*HOSTEL ATTENDANCE TRACKER 
+Assam Down Town University maintains a strict hostel monitoring system to ensure student safety.
+Every hostel student must mark attendance daily.
+The warden keeps attendance for 7 days (Monday to Sunday) for 10 students.
+
+The attendance is stored in a 2D array A[10][7], where:
+A[i][j] = 1 → Student i was Present on Day j
+
+A[i][j] = 0 → Student i was Absent on Day j
+
+Here:
+
+i = Student number (1 to 10)
+
+j = Day number (1 to 7)
+
+The warden wants a weekly summary of attendance.
+
+Your Task: Write the algorithm or C program to do the following:
+(a) Calculate and print the total number of days each student was present.
+
+Total present days = sum of each row.
+
+(b) Identify the student who has the highest attendance in the week.
+
+If two students have the same highest attendance, print the one with the lower student number.
+
+Find the day (1–7) on which overall hostel attendance was the lowest.
+
+Total attendance on a day = sum of each column.
+Input :
+1 1 0 1 1 0 1
+1 0 1 1 1 1 1
+0 1 1 0 1 1 0
+1 1 1 1 0 1 1
+1 0 1 0 1 0 1
+0 0 1 1 1 1 1
+1 1 0 0 0 1 1
+1 1 1 1 1 0 0
+0 1 1 1 1 1 1
+1 0 0 1 1 1 1
+
+Output:
+Weekly Attendance Summary
+-------------------------
+
+Total Present Days:
+Student 1: 5
+Student 2: 6
+Student 3: 4
+Student 4: 6
+Student 5: 4
+Student 6: 5
+Student 7: 5
+Student 8: 5
+Student 9: 6
+Student 10: 5
+
+Student with Highest Attendance: Student 2 (6 Days)
+
+Day with Lowest Overall Attendance: Day 2*/
+
+
+
+
+#include <stdio.h>
+int main() {
+    int st[10], i, maxA = 0, maxS = 1;
+    printf("Enter attendance of 10 students (0 to 7 days):\n\n");
+    for(i = 0; i < 10; i++) {
+        do {
+            printf("Student %d: ", i + 1);
+            scanf("%d", &st[i]);
+
+            if(st[i] < 0 || st[i] > 7)
+                printf("Invalid! Enter value between 0 and 7 only.\n");
+
+        } while(st[i] < 0 || st[i] > 7);
+    }
+    maxA = st[0];
+    for(i = 1; i < 10; i++) {
+        if(st[i] > maxA) {
+            maxA = st[i];
+            maxS = i + 1;
+        }
+    }
+    printf("\nWeekly Attendance Summary\n");
+    printf("\n");
+    for(i = 0; i < 10; i++)
+        printf("Student %d: %d out of 7 days\n", i + 1, st[i]);
+
+    printf("\nHighest Attendance: Student %d (%d days)\n", maxS, maxA);
+
+    return 0;
+}
